1ad373409d6642d0ba2aa882a77194bb18b2bd0c
Cynthia Wasonga <wasonga.cynthia@yahoo.in>
2017-01-18 14:45:37 +0300

    Added HTTP POSt request to public API

--- http_and_web.py
+++ http_and_web.py
@@ -1,5 +1,6 @@
 import requests
 
+
 print("\n\t*** Python HTTP API cmd App ***\n\t%s\n" %("-"*31))
 print("Welcome to this simple Python HTTP API command line app")
 print("Let's get some data from the 'jsonplaceholder.typicode.com' website")
@@ -19,6 +20,15 @@ def get_data(get_url, post_id):
 		print("You need to enter a whole number for your request to succeed: ", e)
 	return "* Nothing to show *"
 
+def post_data(post_url,title, body, userId=11):
+	post_data = {
+		'title': title,
+		'body': body,
+		'userId': userId
+		}
+	r = requests.post(post_url, data=post_data)
+	return r.text
+
 def main():
 	post_id = input("Enter a number between 1 and 100: ")
 	get_url = "https://jsonplaceholder.typicode.com/posts/" + post_id
@@ -26,6 +36,13 @@ def main():
 
 	print("\n\tGetting your request...\n")
 	print(get_data(get_url,post_id))
+
+	print("\nNow let's add a new post...")
+	title = input("Enter a title for your post: ")
+	body = input("Enter a body for your post: ")
+
+	print("\n\tCreating your new post...\n")
+	print(post_data(post_url,title,body))
 
 if __name__ == '__main__':
 	main()
